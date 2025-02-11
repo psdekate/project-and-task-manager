de268cd6fc6d1fa6e1bd25a5487925a0bc5fea94
psdekate <piyushdekate29@gmail.com>
2025-02-11 21:29:10 +0530

    setting up tailwind css

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-// postcss.config.js (ESM version)
-import tailwindcss from "@tailwindcss/postcss";
-import autoprefixer from "autoprefixer";
-
-export default {
-  plugins: [tailwindcss, autoprefixer],
-};
--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="m-0 p-0 box-border">
-        <h1>This is my heading</h1>
+      <div className="m-0 mx-auto box-border w-[90vw] p-4">
+        <h1 className="text-[3rem] font-bold text-amber-600">
+          This is my heading
+        </h1>
       </div>
     </>
   );
